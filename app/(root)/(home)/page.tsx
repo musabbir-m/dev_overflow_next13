f5834e2cb1e7984605e90019c20b57ce637a6cec
musabbir-m <musabbir313@gmail.com>
2024-05-01 08:45:34 +0600

    added auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import { UserButton } from '@clerk/nextjs'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <UserButton showName />
+    </div>
   )
 }
 
